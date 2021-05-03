937df4a74c5493302c35891edfb12cbba0a1c106
c3rb3ru5 <c3rb3ru5@aabbcc.hopto.org>
2021-05-03 07:51:50 -0300

    caliber

---
+++ caliber/caliber.py
@@ -0,0 +1,20 @@
+import logging
+
+from malduck.extractor import Extractor
+from malduck.pe import MemoryPEData
+
+log = logging.getLogger(__name__)
+
+__author__  = "c3rb3ru5"
+__version__ = "1.0.0"
+
+class Caliber(Extractor):
+
+    family     = 'caliber'
+    yara_rules = ('caliber',)
+
+    @Extractor.string
+    def webhooks(self, p, addr, match):
+        # Offset 216451
+        if p.memory:
+            return {'family': 'caliber', 'webhooks': [p.utf16z(addr)]}
