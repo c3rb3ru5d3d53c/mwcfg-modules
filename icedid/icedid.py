60fc2565a5402b865a2b6ef68d7941064f093b00
4rchib4ld <65158158+4rchib4ld@users.noreply.github.com>
2021-05-07 09:17:30 +0200

    Added some DYNAMISM

--- icedid/icedid.py
+++ icedid/icedid.py
@@ -3,6 +3,8 @@ import logging
 from malduck.extractor import Extractor
 from malduck.pe import MemoryPEData
 from malduck.pe import PE
+from malduck import disasm
+from icecream import ic
 log = logging.getLogger(__name__)
 
 __author__  = "4rchib4ld"
@@ -18,7 +20,7 @@ class IcedID(Extractor):
     yara_rules = 'icedid',
 
     @staticmethod
-    def extractPayload(p, count, xorKeyCount):
+    def extractPayload(p):
         # Extracting the payload from the .data section
         MAX_STRING_SIZE = 128
         for section in p.sections:
@@ -27,14 +29,17 @@ class IcedID(Extractor):
                 payload = data[4:MAX_STRING_SIZE].split(b"\0")[0]
                 return payload  
 
-    @Extractor.extractor
-    def obfuscationCode(self, p, addr):
+    @Extractor.rule
+    def icedid(self, p, matches):
+        obfuscationCode = matches.elements["obfuscationCode"][0][2]
+        xorCountValue = obfuscationCode[3] ## Getting this values dynamically because... you never know
+        countValue = obfuscationCode[-1]
         pe_rep = PE(data=p)
         payload = self.extractPayload(pe_rep)
         decrypted = ""
-        for i in range(32):
+        for i in range(countValue):
             try:
-                decrypted += chr(payload[i+64] ^ payload[i])
+                decrypted += chr(payload[i + xorCountValue] ^ payload[i])
             except IndexError:
                 pass
         c2 = decrypted.split("\x00")[0]
